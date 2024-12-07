da333a0863e11e827d44760707f59690e2538727
zidan <adrian.223040151@mail.unpas.ac.id>
2024-12-07 16:38:45 +0700

    landing-page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,31 @@
+        <!-- navbar-start -->
+        <header>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#">
+                        <img src="" alt="logo" />
+                    </a>
+                <ul>
+                    <li>
+                    <a href="index.view.php">Home</a>
+                    <a href="clothes.view.php">Clothes</a>
+                    <a href="shoes.view.php">Shoes</a>
+                    <a href="accesoris.view.php">Accesoris</a>
+                    <a href="allItems.view.php">All Items</a>
+                    </li>
+                </ul>
+                <div class="userChart">
+                    <a href="">
+                    <img src="../img/search.png" alt="search" width="36px" />
+                    </a>
+                    <a href="">
+                    <img src="../img/user.png" alt="user" width="36px" />
+                    </a>
+                    <a href="">
+                    <img src="../img/cart.png" alt="chart" width="36px" />
+                    </a>
+                </div>
+                </div>
+            </nav>
+            </header>
+            <!-- navbar-end -->
