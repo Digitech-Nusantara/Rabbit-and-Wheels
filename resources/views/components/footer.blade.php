1fcb44a9e4b911ad87ccf1db4ffe86a0aea1063d
robbyobet7 <muhamadrobby24@gmail.com>
2025-01-02 23:39:22 +0700

    Landing Page

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,7 +5,7 @@
         <h3>SYRIOUS</h3>
         <p>Bringing the World to Your Doorstep!</p>
         <p>SYRIOUS</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, nostrum!</p>
+        <p>Syrious adalah pilihan tepat untuk Anda yang menginginkan solusi inovatif, efisien, dan berkualitas tinggi. Dengan layanan dan produk yang selalu mengutamakan kepuasan pelanggan, kami memberikan pengalaman yang tidak hanya memuaskan, tetapi juga melebihi harapan. Kelebihan kami terletak pada desain yang modern, teknologi terbaru, dan komitmen untuk terus berinovasi. Setiap produk dari Syrious dirancang dengan perhatian terhadap detail dan kualitas, memberikan nilai lebih yang luar biasa. Percayakan kebutuhan Anda kepada kami, dan nikmati layanan pelanggan yang ramah serta responsif. Syrious, tempat di mana kualitas dan inovasi bertemu untuk memberikan pengalaman terbaik</p>
     
         <div class="icons">
             <a href="https://github.com/Digitech-Nusantara"><i class='bx bxl-github'></i></a>
@@ -15,8 +15,8 @@
     
     <div class="footer-content">
         <h4>Contact Us</h4>
-        <li><a href="#">example@gmail.com</a></li>
-        <li><a href="#">08xxxxxxxxxx</a></li>
+        <li><a href="#">Syrious@gmail.com</a></li>
+        <li><a href="#">0821-2134-2425</a></li>
     </div>
     
     {{-- route sesuai nama yang di hyperlink --}}
